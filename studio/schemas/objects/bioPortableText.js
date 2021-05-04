c52f78b21622d1bfbf17ea15fc6319f02c183acc
Atilio Barreda II <to.atilio@gmail.com>
2021-05-04 02:04:01 -0400

    mp3 names

--- studio/schemas/objects/bioPortableText.js
+++ studio/schemas/objects/bioPortableText.js
@@ -7,12 +7,14 @@ export default {
             type: 'block',
             styles: [{ title: 'Normal', value: 'normal' }],
             lists: [],
+
             marks: {
                 decorators: [
                     { title: 'Strong', value: 'strong' },
                     { title: 'Emphasis', value: 'em' },
                     { title: 'Code', value: 'code' }
-                ]
+                ],
+
             }
         },
         {
