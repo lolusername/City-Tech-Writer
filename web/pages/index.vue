19551e96a5a278e7ebb90eed07c6a0d370fc8bdf
Atilio Barreda II <to.atilio@gmail.com>
2021-05-05 05:54:35 -0400

    style

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,34 +1,71 @@
 <template>
-  <section class="container">
-    <div
-      v-for="(doc, i) in sortedDocs"
-      :key="i"
-      class="row py-2"
-      :class="{ 'justify-content-end': i % 2 }"
-    >
-      <div class="col-10">
-        <div class="row p-3 doc m-1">
-          <section v-if="extractImage(doc.prose_content)" class="col-4 py-2">
-            <img :src="extractImage(doc.prose_content)" class="w-100" alt="" />
-          </section>
-          <section class="col d-flex justify-content-between">
-            <div>
-              <h5>{{ doc.title }}</h5>
-              <div v-for="(author, i) in doc.authors" :key="i" class="h6">
-                {{ author.name }}
-              </div>
-            </div>
-            <div class="sponsor col-5 text-right pr-0">
-              <h6>FACULTY SPONSOR(S)</h6>
+  <div>
+    <header class="container-fluid p-0">
+      <section class="cover"></section>
+      <section class="issue-info container mb-5">
+        <div class="row py-3 d-flex justify-content-center">
+          <article class="col-3 volume">
+            <h4>City Tech Writer</h4>
+            <h4>Vol. 16 - 2021</h4>
+          </article>
+          <article class="col-5">
+            <BlockContent :blocks="issueInfo[0].description" />
+          </article>
+        </div>
+      </section>
+    </header>
+    <section class="container">
+      <div
+        v-for="(doc, i) in sortedDocs"
+        :key="i"
+        class="row py-2"
+        :class="{ 'justify-content-end': i % 2 }"
+      >
+        <div class="col-10">
+          <div class="row p-3 doc m-1">
+            <section v-if="extractImage(doc.prose_content)" class="col-4 py-2">
+              <img
+                :src="extractImage(doc.prose_content)"
+                class="w-100"
+                alt=""
+              />
+            </section>
+            <section class="col d-flex justify-content-between">
               <div>
-                {{ doc.facultySponsor }}
+                <h5>{{ doc.title }}</h5>
+                <div v-for="(author, i) in doc.authors" :key="i" class="h6">
+                  {{ author.name }}
+                </div>
               </div>
-            </div>
+              <div
+                v-if="doc.facultySponsor"
+                class="sponsor col-5 text-right pr-0"
+              >
+                <h6>FACULTY SPONSOR(S)</h6>
+                <div>
+                  {{ doc.facultySponsor }}
+                </div>
+              </div>
+            </section>
+          </div>
+          <section v-if="doc.tags" class="d-flex justify-content-end">
+            <span
+              class="d-inline-block d-flex align-items-center mr-2 tag-header"
+            >
+              Tags:
+            </span>
+            <span
+              class="d-inline-block tag"
+              v-for="(tag, i) in doc.tags"
+              :key="i"
+            >
+              {{ mapTag(tag) }}
+            </span>
           </section>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -38,6 +75,8 @@ import sanityClient from '../sanityClient'
 
 import imageUrlBuilder from '@sanity/image-url'
 
+import BlockContent from 'sanity-blocks-vue-component'
+
 const builder = imageUrlBuilder(sanityClient)
 
 const proseQuery = `
@@ -45,13 +84,19 @@ const proseQuery = `
     "prose": *[_type in ["prose"]] | order(order asc) {
       ...,
        authors[]->,
-       facultySponsor
+       facultySponsor,
+       tags
     }
   }
 `
 const imageGalleryQuery = `
   {
     "imageGalleries": *[_type in ["imageGallery"]] | order(order asc) 
+  }
+`
+const issueInfoQuery = `
+  {
+    "issueInfo": *[_type in ["issueInfo"]]
   }
 `
 
@@ -65,12 +110,13 @@ export default {
       sortedDocs: []
     }
   },
-  computed: {},
+  components: { BlockContent },
   async asyncData() {
     const prose = await sanityClient.fetch(proseQuery)
     const imageGalleries = await sanityClient.fetch(imageGalleryQuery)
+    const issueInfo = await sanityClient.fetch(issueInfoQuery)
 
-    return { ...prose, ...imageGalleries }
+    return { ...prose, ...imageGalleries, ...issueInfo }
   },
   methods: {
     extractImage(blocks = []) {
@@ -81,6 +127,37 @@ export default {
       console.log(imgObject.asset)
 
       return builder.image(imgObject)
+    },
+    toPlainText(blocks = []) {
+      return (
+        blocks
+          // loop through each block
+          .map(block => {
+            console.log(1, block)
+            // if it's not a text block with children,
+            // return nothing
+            if (block._type !== 'block' || !block.children) {
+              return ''
+            }
+            // loop through the children spans, and join the
+            // text strings
+            return block.children.map(child => child.text).join('')
+          })
+          .join('\n\n')
+      )
+    },
+    mapTag(tag) {
+      return {
+        personal_narratives: 'personal narratives',
+        fiction: 'fiction',
+        art_crit: 'art criticism',
+        philosophy: 'philosophy',
+        reflections_healthcare: 'Reflections On Healthcare',
+        tech: 'tech',
+        soc: 'sociology',
+        sci_bi: 'sci-fi',
+        photo: 'photography'
+      }[tag]
     }
   },
   mounted() {
@@ -119,10 +196,52 @@ export default {
   box-sizing: border-box;
   min-height: calc(100% - 72px - 216px);
   font-family: adobe-garamond-pro, serif;
-
+  background: #f1f2f1;
   font-weight: 400;
 
   font-style: normal;
+}
+.volume::after {
+  content: ' ';
+  display: block;
+  background: #214971;
+  border-radius: 100px;
+  width: 75%;
+  height: 5px;
+}
+.issue-info {
+  background: #fafafa;
+  margin-top: 3rem;
+  border-radius: 8px;
+  border: 0.1px solid #000;
+  box-shadow: #214971 10px 10px;
+  font-size: 1.5rem;
+  font-weight: 100;
+  font-style: italic;
+}
+
+.cover {
+  height: 440px;
+  background: url(/banner.png), #214971;
+  background-size: 34vw !important;
+  background-repeat: no-repeat;
+  background-position: center;
+  transition: background 1s;
+}
+.cover:hover {
+  background-position: center -1rem;
+  background-color: #214971;
+  background-size: 120% !important;
+}
+.tag {
+  background: #000;
+  color: #fff;
+  margin: 0 0.25rem;
+  padding: 0.25rem;
+  text-transform: capitalize;
+}
+.tag-header {
+  font-weight: 700;
 }
 h1,
 h2,
@@ -141,6 +260,7 @@ h6 {
 }
 .doc {
   border: 1px solid #000;
+  background: #fafafa;
 }
 .doc img {
   border: 0.5px solid #000;
