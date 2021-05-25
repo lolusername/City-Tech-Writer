05a0e7e70ae48d6560d74d3e666f94f4345048b3
Atilio Barreda II <to.atilio@gmail.com>
2021-05-24 22:59:28 -0400

    favicon and img sizes

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -18,11 +18,10 @@ export default {
     head: {
         title: pkg.name,
         meta: [
-                { charset: 'utf-8' },
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                { hid: 'description', name: 'description', content: pkg.description }
-            ]
-            //link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: pkg.description }
+        ]
     },
 
     /*
